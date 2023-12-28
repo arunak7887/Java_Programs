3305c69d4e58e0b733b0b30e5ddef1eb16101e68
arunak7887 <arunkinwad02@gmail>
2023-12-28 22:27:12 +0530

    First

---
+++ Abstract_Class/Honda4.java
@@ -0,0 +1,15 @@
+package Abstract_Class;
+abstract class Bike {
+    abstract void run();
+}
+public class Honda4 extends Bike {
+    void run(){
+        System.out.println("Running safely");
+    }
+    public static void main(String[] args) {
+        Bike obj=new Honda4();
+        obj.run();
+    }
+
+    
+}
